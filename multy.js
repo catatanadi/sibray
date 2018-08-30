86729d606ed7d2a48b5586426944b9b0561a3a9d
catatanadi <37771855+catatanadi@users.noreply.github.com>
2018-08-30 15:36:42 +0700

    Update multy.js

--- multy.js
+++ multy.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="https://en.sibray.my.id/2018/08/asbestos-lawyers-mesothelioma-lawyers.html"}
-if(x=="2"){xxx="https://en.sibray.my.id/2018/08/car-insurance-quotes-colorado-car.html"}
-if(x=="3"){xxx="https://en.sibray.my.id/2018/08/donate-car-to-charity-california.html"}
-if(x=="4"){xxx="https://en.sibray.my.id/2018/08/how-to-select-mesothelioma-attorney.html"}a_to_ve[i].href="https://en.sibray.my.id"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/08/asbestos-lawyers-mesothelioma-lawyers.html"}
+if(x=="2"){xxx="/2018/08/car-insurance-quotes-colorado-car.html"}
+if(x=="3"){xxx="/2018/08/donate-car-to-charity-california.html"}
+if(x=="4"){xxx="/2018/08/how-to-select-mesothelioma-attorney.html"}a_to_ve[i].href="https://en.sibray.my.id"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
